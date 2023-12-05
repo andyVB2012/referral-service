c8838d4aea43031af6814f0789d3217780250245
Andy <andrewVB2012@protonmail.com>
2023-12-05 14:13:57 -0500

    update to main running http

--- main.go
+++ main.go
@@ -3,108 +3,87 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
 
+	"github.com/andyVB2012/referral-service/http"
+	"github.com/andyVB2012/referral-service/repository"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	// "github.com/andyVB2012/referral-service/proto"
-
 	// pb "github.com/andyVB2012/referral-service/block"
-	"github.com/parsaakbari1209/go-mongo-crud-rest-api/block"
-	"github.com/segmentio/kafka-go"
-	"google.golang.org/protobuf/proto"
+	// "github.com/andyVB2012/referral-service"
 )
 
 func main() {
 	// create a database connection
 
-	// // Use the SetServerAPIOptions() method to set the Stable API version to 1
-	// serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	// // password := "77J8Knd6vrhvVGjL"
-	// opts := options.Client().ApplyURI(
-	// 	"mongodb+srv://user insert here",
-	// ).SetServerAPIOptions(serverAPI)
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	// password := "77J8Knd6vrhvVGjL"
+	opts := options.Client().ApplyURI(
+		"mongodb+srv://andrewvb2012:U3aQdGnoYzznB1d2@cluster0.gmjmvad.mongodb.net/",
+	).SetServerAPIOptions(serverAPI)
 
-	// // Create a new client and connect to the server
-	// client, err := mongo.Connect(context.TODO(), opts)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// defer func() {
-	// 	if err = client.Disconnect(context.TODO()); err != nil {
-	// 		panic(err)
-	// 	}
-	// }()
-
-	// // Send a ping to confirm a successful connection
-	// if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
-
-	// // create a repository
-	// repository := repository.NewRepository(client.Database("stfx-referral"))
-
-	// // create an http server
-	// server := http.NewServer(repository)
-
-	// // create a gin router
-	// router := gin.Default()
-	// {
-	// 	router.GET("/follows/:user1/:user2", server.)
-	// 	router.GET("/follows/:user1", server.GetFollowings)
-	// 	router.GET("/followers/:user2", server.GetFollowers)
-	// 	router.GET("/follows", server.GetAll)
-
-	// 	router.POST("/follows/one", server.CreateFollow)
-	// 	router.POST("/follows/batch", server.CreateFollowBatch)
-	// 	router.DELETE("/follows/:user1/:user2", server.DeleteFollow)
-	// }
-
-	// // start the router
-	// router.Run(":9080")
-
-	// type Consumer struct {
-	// 	reader *kafka.Reader
-	// }
-
-	// make a new reader that consumes from topic-A, partition 0, at offset 42
-	// and only reads new messages
-	// bb,_ := kafka.NewConsumerGroup(
-	// 	kafka.ConsumerGroupConfig{
-	// 		ID:          "my-consumer",
-	// 		Brokers:     []string{"localhost:9092"},
-	// 		Topics:      []string{"stfx.stream.vault"},
-	// 		StartOffset: kafka.LastOffset,
-	// 	},
-	// )
-	// bb.Next(context.Background())
-
-	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", "stfx.stream.block.arb.stfx.vault.v2", 0)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
-	conn.SetReadDeadline(time.Now().Add(100 * time.Second))
 
-	batch := conn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
-	defer batch.Close()
-	count := 0
-
-	for {
-		b := make([]byte, 10e3) // 10KB max per message
-		n, err := batch.Read(b)
-		if err != nil {
-			break // end of batch
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
 		}
-		b = b[:n] // truncate the buffer to the actual message size
+	}()
 
-		var blockEvent block.BlockEvent
-		if err := proto.Unmarshal(b, &blockEvent); err != nil {
-			fmt.Printf("error unmarshalling message: %v", err)
-			continue
-		}
-
-		fmt.Printf("Received message: %+v\n", blockEvent)
-		count++
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
 	}
-	fmt.Println("count", count)
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	// create a repository
+	repository := repository.NewRepository(client.Database("stfx-referral-system"))
+
+	// create an http server
+	server := http.NewServer(repository)
+
+	// res, err := repository.GetAllAttributors(context.Background(), "0x1")
+	// if err != nil {
+	// 	fmt.Println("error: ", err)
+	// }
+	// fmt.Println("Stats: ", res)
+
+	// return
+	// // repository.CreateReferralCode(context.Background(), "0x87ad83dc2f12a14c85d20f178a918a65edfe1b42")
+	// refCode, err := repository.GetCode(context.Background(), "0x87ad83dc2f12a14c85d20f178a918a65edfe1b42")
+	// if err != nil {
+	// 	fmt.Println("error: ", err)
+	// }
+	// fmt.Println("Refcode: ", refCode)
+	// // repository.AddAttributor(context.Background(), refCode, "0x87ad83dc2f12a14c85d20f178a918a65edfe1b42")
+
+	// isIt := repository.IsTraderAddrInDb(context.Background(), "0x87ad83dc2f12a14c85d20f178a918a65edfe1b42")
+	// fmt.Println("Hello", isIt)
+	// kafka.RunnConsumers(repository)
+	// // create a gin router
+	router := gin.Default()
+	{
+		router.GET("/referral-stats/:address", server.GetAttributionStats)
+		router.GET("/referral-attributors/:address", server.GetAllAttributions)
+		router.GET("/referral-code/:address", server.GetReferralCode)
+		// router.GET("/follows/:user1/:user2", server.CreateFollow)
+		// router.GET("/follows/:user1", server.GetFollowings)
+		// router.GET("/followers/:user2", server.GetFollowers)
+		// router.GET("/follows", server.GetAll)
+
+		// router.POST("/follows/one", server.CreateFollow)
+		// router.POST("/follows/batch", server.CreateFollowBatch)
+		// router.DELETE("/follows/:user1/:user2", server.DeleteFollow)
+	}
+
+	// start the router
+	router.Run(":9090")
+
 }
