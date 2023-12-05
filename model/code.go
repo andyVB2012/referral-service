e7ddf7f3f5cf01ad0cf0563dd77a53c937673399
Andy <andrewVB2012@protonmail.com>
2023-12-05 17:04:19 -0500

    removal of uneeded models

--- model/code.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Code struct {
-	TraderAddr string `json:"traderAddr"`
-	Code       string `json:"code"`
-}
--- model/user.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Follow struct {
-	User1 string `json:"user1"`
-	User2 string `json:"user2"`
-}
